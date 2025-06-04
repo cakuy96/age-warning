61877788bb98cc2da4f1d24d291eee016c4cc6e1
cakuy96 <otakkis85@gmail.com>
2025-06-04 16:24:46 +0700

    Create warning.js

---
+++ warning.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (!localStorage.getItem("ageConfirmed")) {
+    document.getElementById("age-warning-overlay").style.display = "flex";
+  }
+
+  document.getElementById("confirmBtn").addEventListener("click", function () {
+    localStorage.setItem("ageConfirmed", "true");
+    document.getElementById("age-warning-overlay").style.display = "none";
+  });
+
+  document.getElementById("denyBtn").addEventListener("click", function () {
+    window.location.href = "/";
+  });
+});
